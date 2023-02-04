8eaf5730ce2710d51999bbbba5d6fcd8719a084e
Sumit Koli <sumitkoli2002@gmail.com>
2023-02-04 12:23:19 +0530

    First commit

---
+++ Car-prediction/import4.py
@@ -0,0 +1,19 @@
+predict = "price"
+data = data[["symboling", "wheelbase", "carlength", 
+             "carwidth", "carheight", "curbweight", 
+             "enginesize", "boreratio", "stroke", 
+             "compressionratio", "horsepower", "peakrpm", 
+             "citympg", "highwaympg", "price"]]
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
+
+from sklearn.tree import DecisionTreeRegressor
+model = DecisionTreeRegressor()
+model.fit(xtrain, ytrain)
+predictions = model.predict(xtest)
+
+from sklearn.metrics import mean_absolute_error
+model.score(xtest, predictions)
